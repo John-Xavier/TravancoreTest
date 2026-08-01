93284e18f42e10439371bb6f724fae27cb9eebe9
John Xavier <94055839+John-Xavier@users.noreply.github.com>
2026-08-01 13:07:46 +0530

    Initial Commit

---
+++ TravancoreTestTests/TravancoreTestTests.swift
@@ -0,0 +1,19 @@
+//
+//  TravancoreTestTests.swift
+//  TravancoreTestTests
+//
+//  Created by John Xavier  on 01/08/2026.
+//
+
+import Testing
+@testable import TravancoreTest
+
+struct TravancoreTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
